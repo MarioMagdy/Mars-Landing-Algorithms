af0e7a224dbdaf09b7eeae07e2a61c19d7f219ec
seif ahmed <161384711+seifaaaa@users.noreply.github.com>
2024-04-09 05:42:27 +0200

    i dont have roll, pitch

--- under_dev/sensors.py
+++ under_dev/sensors.py
@@ -53,28 +53,17 @@ class IMU:
         self.lander = lander
         self.current_acceleration = np.array([0.0, 0.0])
         self.last_velocity = np.array([0.0, 0.0])
-        self.last_euler_angle = np.array([0.0, 0.0])
-        self.current_change_rate_of_euler_angle = np.array([0.0, 0.0])
+        
 
-    def start(self):
-        # Assuming 'get_euler_angles' is a method that retrieves the Euler angles
-        self.last_euler_angle = self.spacecraft_orientation()
 
     def fixed_update(self, delta_time):
         # Accelometer Sensor code & Gyroscope Sensor Code calculated from change rate of velocity vector & change rate of Euler Angles
-        current_euler_angle = self.spacecraft_orientation()
         self.current_acceleration = (vel_vector - self.last_velocity) / delta_time
-        self.current_change_rate_of_euler_angle = (current_euler_angle - self.last_euler_angle) / delta_time
         self.last_velocity = vel_vector
-        self.last_euler_angle = current_euler_angle
 
-    def spacecraft_orientation(self):
-        # Placeholder for getting Euler angles
-        return np.array([0.0, 0.0])  # Dummy values for demonstration
+#calling function
 
 my_imu = IMU("Mars Lander")
-
-my_imu.start()
 
 delta_time = 3 
 
@@ -82,6 +71,4 @@ my_imu.fixed_update(delta_time)
 
 print("Current Acceleration:", my_imu.current_acceleration)
 
-my_imu.fixed_update(delta_time)
 
-print("current_change_rate_of_euler_angle", my_imu.current_change_rate_of_euler_angle)
